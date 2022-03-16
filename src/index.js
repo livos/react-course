db1ccacee5a5413cdba451f35faa17a8153caa73
lvicos <lvivancos2nd-dev@yahoo.fr>
2022-03-16 20:24:52 +0700

    nested components

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import ReactDOM from 'react-dom'
 
+// Nested components, React tools
+
 const Greeting = () => {
   return (
     <div>
-    <Person/>
-    <Message/>
+      <Person />
+      <Message />
     </div>
   )
 }
 
-const Person = () => <h2>John Doe</h2>
-const Message = () => <h3>Hello world</h3>
+const Person = () => <h2>john doe</h2>
+const Message = () => {
+  return <p>this is my message</p>
+}
 
 
 ReactDOM.render(<Greeting />, document.getElementById('root'))
