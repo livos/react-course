70f4b36691ffe2dea4e4f5db8493f31b8e9aae1e
livos <you@example.com>
2022-03-17 17:24:56 +0700

    add css in jsx

--- src/index.js
+++ src/index.js
@@ -32,7 +32,7 @@ const Book = () => {
 
 // implicit return
 const Image = () => <img src="https://images-na.ssl-images-amazon.com/images/I/91AfEwKjDgL._AC_UL604_SR604,400_.jpg" alt=""/>
-const Author = () => <h4>Don Miguel Ruiz</h4>
+const Author = () => <h4 style={{ color:'#617d98', fontSize:'0.75rem', marginTop:'0.25rem'}}>Don Miguel Ruiz</h4>
 const Title = () => <h1>The Four Agreements: A Practical Guide to Personal Freedom</h1>
 
 
