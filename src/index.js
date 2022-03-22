40f23811bf3270c3c76f447873623071abd53d87
lvicos <lvivancos2nd-dev@yahoo.fr>
2022-03-22 16:20:33 +0700

    props

--- src/index.js
+++ src/index.js
@@ -2,26 +2,41 @@ import ReactDOM from 'react-dom'
 
 // CSS
 import './index.css'
+// setup vars
+const firstBook = {
+  img: "https://images-na.ssl-images-amazon.com/images/I/91AfEwKjDgL._AC_UL604_SR604,400_.jpg",
+  title: "The Four Agreements: A Practical Guide to Personal Freedom",
+  author: "Don Miguel Ruiz"
+}
+
+const secondBook = {
+  img: "https://m.media-amazon.com/images/I/41e+bOAeE-L._SY346_.jpg",
+  title: "The Maid: A Novel",
+  author: "Nita Prose"
+}
 
 const BookList = () => {
   return (
     <section className='booklist'>
-      <Book />
+      <Book 
+        img={firstBook.img} 
+        title={firstBook.title} 
+        author={firstBook.author}/>
+      <Book 
+        img={secondBook.img} 
+        title={secondBook.title} 
+        author={secondBook.author}/>
     </section>
   )
 }
 
 // explicit return
-const author = "Don Miguel Ruiz"
-const Book = () => {
-  const title = "The Four Agreements: A Practical Guide to Personal Freedom"
+const Book = (props) => {  
   return (
     <article className='book'>
-      <img src="https://images-na.ssl-images-amazon.com/images/I/91AfEwKjDgL._AC_UL604_SR604,400_.jpg" alt=""/>
-      <h1>{title}</h1>
-      <h4>{author.toUpperCase()}</h4>
-      {/* <p>{something with comment}</p> */}
-      <p>{6 + 6}</p>
+      <img src={props.img} alt=""/>
+      <h1>{props.title}</h1>
+      <h4>{props.author}</h4>
     </article>
   )
 }
