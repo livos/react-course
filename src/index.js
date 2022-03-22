30008a6a53460b22bbcaa916ef373b2cf821512c
lvicos <lvivancos2nd-dev@yahoo.fr>
2022-03-22 18:06:54 +0700

    map iteration and key prop

--- src/index.js
+++ src/index.js
@@ -2,45 +2,45 @@ import ReactDOM from 'react-dom'
 
 // CSS
 import './index.css'
-// setup vars
-const firstBook = {
-  img: "https://images-na.ssl-images-amazon.com/images/I/91AfEwKjDgL._AC_UL604_SR604,400_.jpg",
-  title: "The Four Agreements: A Practical Guide to Personal Freedom",
-  author: "Don Miguel Ruiz"
-}
-
-const secondBook = {
-  img: "https://m.media-amazon.com/images/I/41e+bOAeE-L._SY346_.jpg",
-  title: "The Maid: A Novel",
-  author: "Nita Prose"
-}
+const books = [
+  {
+    id:1,
+    img: "https://images-na.ssl-images-amazon.com/images/I/91AfEwKjDgL._AC_UL604_SR604,400_.jpg",
+    title: "The Four Agreements: A Practical Guide to Personal Freedom",
+    author: "Don Miguel Ruiz"
+  }, 
+  {
+    id:2,
+    img: "https://m.media-amazon.com/images/I/41e+bOAeE-L._SY346_.jpg",
+    title: "The Maid: A Novel",
+    author: "Nita Prose"
+  }, 
+  {
+    id:3,
+    img: "https://m.media-amazon.com/images/I/41EiBLxybSL.jpg",
+    title: "Reminders of Him: A Novel",
+    author: "Colleen Hoover"
+  }  
+]
 
 const BookList = () => {
   return (
     <section className='booklist'>
-      <Book 
-        img={firstBook.img} 
-        title={firstBook.title} 
-        author={firstBook.author}>
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci illum ut quod ea quo nostrum, iusto ullam deserunt molestiae minus.</p>
-        </Book>
-      <Book 
-        img={secondBook.img} 
-        title={secondBook.title} 
-        author={secondBook.author}/>
-    </section>
+      {books.map((book) => {
+        const { img, title, author } = book      
+        return <Book key={book.id} book={book}></Book>
+    })}</section>
   )
 }
 
 // explicit return
-const Book = (props) => {  
-  const {img, title, author} = props
+const Book = (props) => {
+  const {img, title, author} = props.book
   return (
     <article className='book'>
       <img src={img} alt=""/>
       <h1>{title}</h1>
       <h4>{author}</h4>
-      { props.children }
     </article>
   )
 }
