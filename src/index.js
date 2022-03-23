c7443914920249254f6626e3bc9ccf1242de54be
lvicos <lvivancos2nd-dev@yahoo.fr>
2022-03-23 17:27:11 +0700

    reference

--- src/index.js
+++ src/index.js
@@ -2,16 +2,17 @@ import ReactDOM from 'react-dom'
 
 // CSS
 import './index.css'
+
 const books = [
   {
     id:1,
-    img: "https://images-na.ssl-images-amazon.com/images/I/91AfEwKjDgL._AC_UL604_SR604,400_.jpg",
+    img: "https://images-na.ssl-images-amazon.com/images/I/51oVrNQVLiL._SX345_BO1,204,203,200_.jpg",
     title: "The Four Agreements: A Practical Guide to Personal Freedom",
     author: "Don Miguel Ruiz"
   }, 
   {
     id:2,
-    img: "https://m.media-amazon.com/images/I/41e+bOAeE-L._SY346_.jpg",
+    img: "https://m.media-amazon.com/images/I/41e+bOAeE-L.jpg",
     title: "The Maid: A Novel",
     author: "Nita Prose"
   }, 
@@ -27,20 +28,24 @@ const BookList = () => {
   return (
     <section className='booklist'>
       {books.map((book) => {
-        const { img, title, author } = book      
+        const { img, title, author } = book
         return <Book key={book.id} {...book}></Book>
     })}</section>
   )
 }
 
-// explicit return
 const Book = ({ img, title, author }) => {
-  // const {img, title, author} = props.book
+  const clickHandler = () => {
+    alert('hello world')
+  }
   return (
     <article className='book'>
       <img src={img} alt=""/>
       <h1>{title}</h1>
       <h4>{author}</h4>
+      <button type="button" onClick={clickHandler}>
+        reference example
+      </button>
     </article>
   )
 }
